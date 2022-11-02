4b3ccdb7b234c7bec5ac4658b4b9c7839462e7f7
SriTarun Karuturi <108359845+sritarun99@users.noreply.github.com>
2022-11-02 18:02:14 -0500

    all files updated

--- README.md
+++ README.md
@@ -1,3 +1,14 @@
 # f2wb37karuturi
 Created my own server
 <a href="https://f2wb37karuturi.onrender.com/">Link to render</a>
+class India{
+    String State1;
+    String State2;
+    String State3;
+    public(String State1,String State2,String State3)
+    {
+        this.State1=State1;
+        this.State2=State2;
+        this.State3=State3;
+    }
+ }
--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var IndiaRouter = require('./routes/India');
+var gridbuildRouter = require('./routes/gridbuild');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/India', IndiaRouter);
+app.use('/selector', selectorRouter);
+app.use('/gridbuild', gridbuildRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
